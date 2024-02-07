9830b1905f6a99a03e49a25c9ff1c9f671353ce6
Ray Olivier <rayolivier@outlook.com>
2024-02-07 12:38:40 -0600

    convert project to typescript

---
+++ pages/chat.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Chat() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS ChatGPT Starter</title>
+      </Head>
+      <h1>Welcome to the Chat page</h1>
+    </div>
+  );
+}
