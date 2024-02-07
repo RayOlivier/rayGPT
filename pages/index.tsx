e24688a2455a54e9b15d75f0921430d16a43a3fd
Ray Olivier <rayolivier@outlook.com>
2024-02-07 17:47:11 -0600

    create sign up button and home styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
 
 export default function Home() {
   const { isLoading, error, user } = useUser();
@@ -9,18 +10,47 @@ export default function Home() {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>Next Chat</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-      <div>
-        {!user ? (
-          <Link href={"/api/auth/login"}>Login</Link>
-        ) : (
-          <Link href={"/api/auth/logout"}>Logout</Link>
-        )}
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
+        <div>
+          {!user ? (
+            <>
+              <Link
+                href={"/api/auth/login"}
+                className="btn"
+              >
+                Login
+              </Link>
+              <Link
+                href={"/api/auth/signup"}
+                className="btn"
+              >
+                Sign Up
+              </Link>
+            </>
+          ) : (
+            <Link href={"/api/auth/logout"}>Logout</Link>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx.req, ctx.res);
+  if (!!session) {
+    return {
+      redirect: {
+        destination: "/chat",
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
