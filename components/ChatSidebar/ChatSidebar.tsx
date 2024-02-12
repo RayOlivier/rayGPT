0457b10d29b87462c5ce4ee11fba55b16d8168f9
Ray Olivier <rayolivier@outlook.com>
2024-02-12 13:21:42 -0600

    load previous chats in sidebar

--- components/ChatSidebar/ChatSidebar.tsx
+++ components/ChatSidebar/ChatSidebar.tsx
@@ -1,9 +1,52 @@
+import {
+  faMessage,
+  faPlus,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const ChatSidebar = () => {
+  const [chatList, setChatList] = useState([]);
+
+  useEffect(() => {
+    const loadChatList = async () => {
+      const response = await fetch("/api/chat/getChatList", { method: "POST" });
+      const json = await response.json();
+      console.log("json", json);
+      setChatList(json?.chats || []);
+    };
+    loadChatList();
+  }, []);
+
   return (
-    <div className="bg-zinc-900">
-      <Link href="/api/auth/logout">Logout</Link>
+    <div className="flex flex-col overflow-hidden bg-zinc-900 text-white">
+      <Link
+        className="side-menu-item bg-emerald-500 hover:bg-emerald-600"
+        href="/chat"
+      >
+        <FontAwesomeIcon icon={faPlus} />
+        New Chat
+      </Link>
+      <div className="flex-1 overflow-auto bg-zinc-950">
+        {chatList.map((chat) => {
+          return (
+            <Link
+              className="side-menu-item"
+              key={chat._id}
+              href={`/chat/${chat._id}`}
+            >
+              <FontAwesomeIcon icon={faMessage} />
+              {chat.title}
+            </Link>
+          );
+        })}
+      </div>
+      <Link className="side-menu-item" href="/api/auth/logout">
+        <FontAwesomeIcon icon={faRightFromBracket} />
+        Logout
+      </Link>
     </div>
   );
 };
